571b7eba0da931d84b9ab8d80ed99ad723f00002
郝洋洋 <yangyang_hao@fpi-inc.com>
2019-08-05 19:40:42 +0800

    完整版

--- src/index.js
+++ src/index.js
@@ -51,10 +51,11 @@ class Game extends React.Component {
           squares: Array(9).fill(null)
         }
       ],
-      xIsNext: true,
-      stepNumber: 0
+      stepNumber: 0,
+      xIsNext: true
     };
   }
+
   handleClick(i) {
     const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
@@ -73,40 +74,39 @@ class Game extends React.Component {
       xIsNext: !this.state.xIsNext
     });
   }
+
   jumpTo(step) {
     this.setState({
       stepNumber: step,
       xIsNext: step % 2 === 0
     });
   }
+
   render() {
     const history = this.state.history;
     const current = history[this.state.stepNumber];
     const winner = calculateWinner(current.squares);
+
     const moves = history.map((step, move) => {
       const desc = move ? "Go to move #" + move : "Go to game start";
       return (
-        <li key={step}>
-          <button
-            onClick={() => {
-              this.jumpTo(move);
-            }}
-          >
-            {desc}
-          </button>
+        <li key={move}>
+          <button onClick={() => this.jumpTo(move)}>{desc}</button>
         </li>
       );
     });
+
     let status;
     if (winner) {
       status = "Winner: " + winner;
     } else {
-      status = "Next Player: " + (this.state.xIsNext ? "X" : "O");
+      status = "Next player: " + (this.state.xIsNext ? "X" : "O");
     }
+
     return (
       <div className="game">
         <div className="game-board">
-          <Board />
+          <Board squares={current.squares} onClick={i => this.handleClick(i)} />
         </div>
         <div className="game-info">
           <div>{status}</div>
